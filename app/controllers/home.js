b8f731c1a2c332db1dc8dc8e55765d19958aa5aa
Kenneth Truyers <kenneth@kenneth-truyers.net>
2016-09-06 01:13:31 +0100

    Added IP output

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   router = express.Router(),
-  Article = require('../models/article');
+  Article = require('../models/article'),
+  os = require('os');;
 
 module.exports = function (app) {
   app.use('/', router);
@@ -10,6 +11,12 @@ router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
     res.render('index', {
       title: 'Generator-Express MVC',
-      articles: articles
+      articles: articles,
+      ip: getIp()
     });
 });
+
+const getIp = () => 
+  Object.keys(os.networkInterfaces())
+        .map(ifname => ifaces[ifname].filter(iface => 'IPv4' === iface.family && iface.internal)
+                                                                      .map(iface => iface.address)[0])[0];
